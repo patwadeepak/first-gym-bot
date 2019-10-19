d5fc56e76e6aef935ed77c6e5e885f01e9fabd55
Deepak <patwa.deepak@gmail.com>
2019-10-19 15:04:37 +0530

    Cartpole is moving now... yeah :)

--- testrun.py
+++ testrun.py
@@ -3,6 +3,14 @@ import pybulletgym
 
 env = gym.make('CartPole-v0')
 env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample())
+for i_episode in range(20):
+    observation = env.render()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+
+        if done:
+            print("episode finished after {} timesteps".format(t))
+            break
